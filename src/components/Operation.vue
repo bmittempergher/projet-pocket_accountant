4ec61d1dbc97f748ed8eba2ef1873e3a7ebe0160
Bastien Mittempergher <bastien.mittempergher@he-arc.ch>
2017-07-05 19:45:39 +0200

    Création vue Transactions et Opération 	Transactions : 			+ Récupération de la liste de transactions 			+ Parcours des éléments de la liste 			+ Envoie des données à Operation.vue 	Opération : 			+ Récupération des éléments via props 			+ Lecture et affichage des éléments 			+ Mise en page avec bootstrap (Grid,...) 	Index : 			+ Ajout d'un container global

---
+++ src/components/Operation.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <li class="list-group-item">
+      <div class="row">
+        <div class="col-xs-2">
+          {{ ope.date }}
+        </div>
+        <div class="col-xs-3">
+          {{ ope.categorie }}
+        </div>
+        <div class="col-xs-3">
+          {{ ope.note }}
+        </div>
+        <div class="col-xs-2">
+          {{ ope.montant }}
+        </div>
+        <div class="col-xs-2">
+          <button v-on:click="$emit('Delete', person)" class="btn btn-danger">x</button>
+        </div>
+      </div>  
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['ope'],
+    data () {
+        return {
+            msg: 'Operation'
+        };
+    }
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
