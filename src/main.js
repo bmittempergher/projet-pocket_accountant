9dfc157bcb171bcdf7336435cce177c2121ae1a7
valentindubois <valentin.dubois2@he-arc.ch>
2017-07-05 18:04:23 +0200

    Ajout des données de base dans main.js

--- src/main.js
+++ src/main.js
@@ -6,10 +6,48 @@ import router from './router';
 
 Vue.config.productionTip = false;
 
+const LOCALSTORAGE_KEY_TRANSACTIONS = 'transactions';
+
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
     template: '<App/>',
-    components: { App }
+    components: { App },
+    data () {
+        let listTransactions;
+        try {
+            const jsonList = localStorage.getItem(LOCALSTORAGE_KEY_TRANSACTIONS);
+            listTransactions = JSON.parse(jsonList);
+            if (!listTransactions) {
+                throw new Error();
+            }
+        } catch (e) {
+            listTransactions = [
+              { date: '05.07.2017', categorie: 'Restaurant', note: 'McDonald', montant: 16.80 },
+              { date: '06.07.2017', categorie: 'Loisir', note: 'Jeu', montant: 20.80 },
+              { date: '08.07.2017', categorie: 'Habillement', note: 'Pull', montant: 40.80 }
+            ];
+        }
+        return {
+            budget: 400.00,
+            listTransactions,
+            listCategories: [
+              { nom: 'Alimentaire', couleur: '#FF0000', image: 'url' },
+              { nom: 'Loisirs', couleur: '#00FF00', image: 'url' },
+              { nom: 'Logement', couleur: '#0000FF', image: 'url' },
+              { nom: 'Transport', couleur: '#FF0000', image: 'url' },
+              { nom: 'Habillement', couleur: '#FF0000', image: 'url' },
+              { nom: 'Divers', couleur: '#FF0000', image: 'url' }
+            ]
+        };
+    },
+    watch: {
+        listTransactions: {
+            handler: function (val, oldVal) {
+                localStorage.setItem(LOCALSTORAGE_KEY_TRANSACTIONS, JSON.stringify(this.listTransactions, null, 2));
+            },
+            deep: true
+        }
+    }
 });
