62b2df2c4c549d5bb5b1bbc152b68be645026674
Bastien Mittempergher <bastien.mittempergher@he-arc.ch>
2017-07-06 01:00:08 +0200

    Ajout de librairie, modification de l'affichage du Nouveau.vue, ajout d'icone 	Librairie : 		- Correction import Bootstrap et Bootswatch 		- Ajout jQuery 			+ Paramètrage dans webpack.dev.conf.js 			+ Paramètrage dans webpack.prod.conf.js 	main.js : 		- Ajout des liens vers les icones 		- Ajout des lignes d'import 	Nouveau.vue : 		- Ajout des catégories via la liste de catégorie créée dans main.js 		- Insertion des icones 		- Mise en page

--- src/main.js
+++ src/main.js
@@ -1,68 +1,80 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import $ from 'jquery';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import '../node_modules/bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootswatch/cosmo/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
 const LOCALSTORAGE_KEY_TRANSACTIONS = 'transactions';
 
 /* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    router,
-    template: '<App/>',
-    components: { App },
-    data () {
-        let listTransactions;
-        try {
-            const jsonList = localStorage.getItem(LOCALSTORAGE_KEY_TRANSACTIONS);
-            listTransactions = JSON.parse(jsonList);
-            if (!listTransactions) {
-                throw new Error();
-            }
-        } catch (e) {
-            listTransactions = [
-              { date: '05.07.2017', categorie: 'Restaurant', note: 'McDonald', montant: 16.80 },
-              { date: '06.07.2017', categorie: 'Loisir', note: 'Jeu', montant: 20.80 },
-              { date: '08.07.2017', categorie: 'Habillement', note: 'Pull', montant: 40.80 }
-            ];
-        }
-        return {
-            budget: 400.00,
-            listTransactions,
-            listCategories: [
-              { nom: 'Alimentaire', couleur: '#FF0000', image: 'url' },
-              { nom: 'Loisirs', couleur: '#00FF00', image: 'url' },
-              { nom: 'Logement', couleur: '#0000FF', image: 'url' },
-              { nom: 'Transport', couleur: '#FF0000', image: 'url' },
-              { nom: 'Habillement', couleur: '#FF0000', image: 'url' },
-              { nom: 'Divers', couleur: '#FF0000', image: 'url' }
-            ]
-        };
-    },
-    watch: {
-        listTransactions: {
-            handler: function (val, oldVal) {
-                localStorage.setItem(LOCALSTORAGE_KEY_TRANSACTIONS, JSON.stringify(this.listTransactions, null, 2));
-            },
-            deep: true
-        }
-    },
-    computed: {
-        depenses: function () {
-            return this.listTransactions.reduce((total, transaction) => total + transaction.montant, 0);
-        },
-        listGroupe () {
-            return this.listTransactions.reduce((totauxCat, transaction) => {
-                if (!totauxCat.hasOwnProperty(transaction.categorie)) {
-                    totauxCat[transaction.categorie] = 0;
+$(document).ready(() => {
+    new Vue({
+        el: '#app',
+        router,
+        template: '<App/>',
+        components: { App },
+        data () {
+            let listTransactions;
+            try {
+                const jsonList = localStorage.getItem(LOCALSTORAGE_KEY_TRANSACTIONS);
+                listTransactions = JSON.parse(jsonList);
+                if (!listTransactions) {
+                    throw new Error();
                 }
-                totauxCat[transaction.categorie] += transaction.montant;
-                return totauxCat;
-            }, {});
+            } catch (e) {
+                listTransactions = [
+                { date: '05.07.2017', categorie: 'Alimentaire', note: 'McDonald', montant: 16.80 },
+                { date: '06.07.2017', categorie: 'Loisir', note: 'Jeu', montant: 20.80 },
+                { date: '05.07.2017', categorie: 'Alimentaire', note: 'SubWay', montant: 11.20 },
+                { date: '06.07.2017', categorie: 'Loisir', note: 'Piscine', montant: 12.80 },
+                { date: '05.07.2017', categorie: 'Alimentaire', note: 'McDonald', montant: 16 },
+                { date: '06.07.2017', categorie: 'Loisir', note: 'Jeu', montant: 20.80 },
+                { date: '05.07.2017', categorie: 'Transport', note: 'AG', montant: 68 },
+                { date: '06.07.2017', categorie: 'Divers', note: 'Facture salt', montant: 39 },
+                { date: '08.07.2017', categorie: 'Habillement', note: 'Pull', montant: 40.80 }
+                ];
+            }
+            return {
+                budget: 400.00,
+                listTransactions,
+                listCategories: [
+                { nom: 'Alimentaire', couleur: '#FF0000', image: '../static/img/icon_alimentaire.jpg' },
+                { nom: 'Loisirs', couleur: '#00FF00', image: '../static/img/icon_loisir.jpg' },
+                { nom: 'Logement', couleur: '#0000FF', image: '../static/img/icon_logement.jpg' },
+                { nom: 'Transport', couleur: '#FF0000', image: '../static/img/icon_transport.jpg' },
+                { nom: 'Habillement', couleur: '#FF0000', image: '../static/img/icon_habit.jpg' },
+                { nom: 'Divers', couleur: '#FF0000', image: '../static/img/icon_divers.jpg' }
+                ]
+            };
+        },
+        watch: {
+            listTransactions: {
+                handler: function (val, oldVal) {
+                    localStorage.setItem(LOCALSTORAGE_KEY_TRANSACTIONS, JSON.stringify(this.listTransactions, null, 2));
+                },
+                deep: true
+            }
+        },
+        computed: {
+            depenses: function () {
+                return this.listTransactions.reduce((total, transaction) => total + transaction.montant, 0);
+            },
+            listGroupe () {
+                return this.listTransactions.reduce((totauxCat, transaction) => {
+                    if (!totauxCat.hasOwnProperty(transaction.categorie)) {
+                        totauxCat[transaction.categorie] = 0;
+                    }
+                    totauxCat[transaction.categorie] += transaction.montant;
+                    return totauxCat;
+                }, {});
+            }
         }
-    }
+    });
 });
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
 import Depenses from '@/components/Depenses';
 import Nouveau from '@/components/Nouveau';
 import Graphique from '@/components/Graphique';
@@ -12,11 +11,6 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'Hello',
-            component: Hello
-        },
-        {
-            path: '/depenses/',
             name: 'Depenses',
             component: Depenses
         },
